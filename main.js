c93b42563c8452199f16b54ce741fb7e04827c17
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-04-10 01:14:29 -0300

    logica dos botoes

--- main.js
+++ main.js
@@ -14,13 +14,23 @@ function receiveNumberOfPeopleValue() {
 
 function receiveTipPercentageValue(value) {
     tipPerecentage = value/100
+
+    removeClassButtonSelected()
+    document.querySelector("custom-tip").value=""
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
    
 }
+function receiveCustomTipPercentageValue(){
+    tipPerecentage = document;querySelector("#custom-tip").valueAsNumber /100
+    removeClassButtonSelected()
+}
+
+
 function removeClassButtonSelected() {
     if(buttonSelected !== null) {
         buttonSelected.classList.remove ("button-selected")
+        buttonSelected = null
 
    }
 }
