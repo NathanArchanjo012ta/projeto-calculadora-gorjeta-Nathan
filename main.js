6a0627cbb41b67ad585efaeac63abf155b2c2e9d
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-04-10 01:07:37 -0300

    criação da funcção para remover o button selected

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPerecentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 function receiveBillValue() {
     bill= document.querySelector("#bill").valueAsNumber
@@ -9,4 +10,17 @@ function receiveBillValue() {
 function receiveNumberOfPeopleValue() {
     numberOfPeople = document.querySelector("#people").valueAsNumber
    
+}
+
+function receiveTipPercentageValue(value) {
+    tipPerecentage = value/100
+    buttonSelected = document.querySelector(`#button-${value}`)
+    buttonSelected.classList.add("button-selected")
+   
+}
+function removeClassButtonSelected() {
+    if(buttonSelected !== null) {
+        buttonSelected.classList.remove ("button-selected")
+
+   }
 }
