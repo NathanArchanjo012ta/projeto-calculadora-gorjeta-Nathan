1af4707f075da41131afce9a011983f62fccb25f
NathanArchanjo012ta <nathanaccouti21@gmail.com>
2025-04-10 00:51:33 -0300

    criação do java

---
+++ main.js
@@ -0,0 +1,12 @@
+let bill = 0
+let tipPerecentage = 0
+let numberOfPeople = 0
+
+function receiveBillValue() {
+    bill= document.querySelector("#bill").valueAsNumber
+    console.log(bill)
+}
+function receiveNumberOfPeopleValue() {
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+   
+}
